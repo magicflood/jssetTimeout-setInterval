234f006bda498fdc1c7fa3aa7de9084595ac2c6c
SaidamirRahimtorayev <saidamirxonrahimtorayev@gmail.com>
2024-06-25 10:09:30 +0500

    first commit

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-let uzbData = new Date();
-let watch = uzbData.getHours();
-let minut = uzbData.getMinutes();
-let data = uzbData.getDate();
-let sec = uzbData.getSeconds();
-let texthour = document.getElementById("hour");
-let textmin = document.getElementById("min");
-let textsec = document.getElementById("sec");
-let textdata = document.getElementById("data");
+function datefunc() {
+    let uzbData = new Date();
+    let watch = uzbData.getHours();
+    let minut = uzbData.getMinutes();
+    let data = uzbData.getDate();
+    let sec = uzbData.getSeconds();
+    let texthour = document.getElementById("hour");
+    let textmin = document.getElementById("min");
+    let textsec = document.getElementById("sec");
+    let textdata = document.getElementById("data");
 
-textdata.innerHTML = data;
-texthour.innerHTML = watch;
-textmin.innerHTML = minut;
-textsec.innerHTML = sec;
-textdata.innerHTML = `${uzbData.getDate()}, ${uzbData.getFullYear()}`
+    textdata.innerHTML = data;
+    texthour.innerHTML = watch;
+    textmin.innerHTML = minut;
+    textsec.innerHTML = sec;
+    textdata.innerHTML = `${uzbData.getDate()}, ${uzbData.getFullYear()}`
+}
+
+setInterval(datefunc, 1000)
